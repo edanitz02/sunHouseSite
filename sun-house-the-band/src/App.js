0a16f47d845abc0809e0e944ed484fef167c0c1e
EthanD <edanitz@zagmail.gonzaga.edu>
2025-06-04 14:32:30 -0700

    Removing subfolder, so it's at first layer

--- sun-house-the-band/src/App.js
+++ sun-house-the-band/src/App.js
@@ -1,21 +1,15 @@
 import './App.css';
 import PlanetMap from './PlanetMap.js';
 import Cursor from './Cursor.js';
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+
 
 function App() {
   return (
-    <BrowserRouter>
+    <div>
       <Cursor />
       <BandName />
       <PlanetMap />
-      <Routes>
-        <Route path="/" element={<>Hi</>}/>
-        <Route path="/grill-info" element={<>What</>}/>
-        <Route path="/rent-grill" element={<>Nah That is wild</>}/>
-        <Route path="*" element={<h1>404 Not Found</h1>}/>
-      </Routes>
-    </BrowserRouter>
+    </div>
   );
 }
 
